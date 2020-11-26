b4b530dc1c8d61cd956ccd6b00ad6b3675463955
alexkobrin <storyday2012@gmail.com>
2020-11-26 11:02:08 +0100

    added vue config

---
+++ dist/precache-manifest.73591e113bf2db33bd4bb2531b18ed4b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dcadf15cdc5c492237",
+    "url": "/css/app.4cd6f013.css"
+  },
+  {
+    "revision": "1698e9434f2c8850b7eb",
+    "url": "/css/chunk-52183eaa.f76690c8.css"
+  },
+  {
+    "revision": "aa0abcd8ed4936c71448",
+    "url": "/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "584b279df45010ff26948b9d6ae0c6b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5dcadf15cdc5c492237",
+    "url": "/js/app.7e0b4058.js"
+  },
+  {
+    "revision": "e2502237948663278876",
+    "url": "/js/chunk-0d50b56f.4c82f910.js"
+  },
+  {
+    "revision": "5d852d969d1f4584acb0",
+    "url": "/js/chunk-10b86dd9.2f19e47a.js"
+  },
+  {
+    "revision": "09c71f9169488f6590cb",
+    "url": "/js/chunk-2d20741e.e210d158.js"
+  },
+  {
+    "revision": "ef10201de84a7664fdcb",
+    "url": "/js/chunk-2d21a3d2.07ea5b5b.js"
+  },
+  {
+    "revision": "1698e9434f2c8850b7eb",
+    "url": "/js/chunk-52183eaa.e28c601b.js"
+  },
+  {
+    "revision": "63c9cfff2806032761ee",
+    "url": "/js/chunk-53c8ec65.b7b9f17a.js"
+  },
+  {
+    "revision": "44af2897a43743825eae",
+    "url": "/js/chunk-5f4ad635.7577f019.js"
+  },
+  {
+    "revision": "90c9620246b4d84c0107",
+    "url": "/js/chunk-72449522.d7b56fc7.js"
+  },
+  {
+    "revision": "c8dabbd339f56ccec088",
+    "url": "/js/chunk-c420df12.d3e00e1d.js"
+  },
+  {
+    "revision": "aa0abcd8ed4936c71448",
+    "url": "/js/chunk-vendors.b710fa0e.js"
+  },
+  {
+    "revision": "30867824803d30949a5f6ba485cacce9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
